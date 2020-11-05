825e670e40cd6129f0f41e682a089f94b8a5bb14
Shariffuddin-Hussin <shariffuddin.hussin@inx.my>
2020-11-06 07:02:08 +0800

    add login page

---
+++ src/components/auth/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="login-form">
+    <h2 class="login-heading">Login</h2>
+    <form action="">
+      <div class="form-control">
+        <label for="Username">Username</label>
+        <input type="text" name="username" id="username" class="login-input" />
+      </div>
+      <div class="form-control mb-more"></div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
